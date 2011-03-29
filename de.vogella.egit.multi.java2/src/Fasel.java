62f626601c6b8e8807f96e9996f0299f8b8bb300
Marek <chefkoch666@googlemail.com>
2011-03-29 20:38:14 +0200

    first commit

---
+++ de.vogella.egit.multi.java2/src/Fasel.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author speedy
+ *
+ */
+public class Fasel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
